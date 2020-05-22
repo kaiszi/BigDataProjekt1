dd549f01092becdf4a6755a8e1077477717df217
kaiszi <kgolis97@gmail.com>
2020-05-22 14:46:34 +0200

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -8,12 +8,12 @@ for input_line in sys.stdin:
     keystring = l[0]
     if keystring not in output.keys():
         if l[1] == "actor" or l[1] == "actress":
-            output[keystring] = int(l[2]) + "\t" + '0'
-        else: output[keystring] = '0' + "\t" + int(l[2])
+            output[keystring] = l[2] + "\t" + '0'
+        else: output[keystring] = '0' + "\t" + l[2]
     else:
         if l[1] == "actor" or l[1] == "actress":
-            output[keystring] = int(l[2]) + "\t" + int(output[keystring][2])
-        else: output[keystring] = int(output[keystring][0]) + "\t" + int(l[2])
+            output[keystring] = l[2] + "\t" + output[keystring][2]
+        else: output[keystring] = output[keystring][0] + "\t" + l[2]
 
 
 for key in output:
