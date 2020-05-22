d80cb80eb685f7d739c381baca20ed6580810798
kaiszi <kgolis97@gmail.com>
2020-05-22 16:40:20 +0200

    Update skrypt1.sh

--- skrypt1.sh
+++ skrypt1.sh
@@ -2,7 +2,7 @@ wget http://www.cs.put.poznan.pl/kjankiewicz/bigdata/projekt1/title.principals.t
 wget https://raw.githubusercontent.com/kaiszi/BigDataProjekt1/master/mapper.py
 wget https://raw.githubusercontent.com/kaiszi/BigDataProjekt1/master/reducer.py
 gunzip title.principals.tsv.gz
-sed -i 5000,+9999999999999999d title.principals.tsv
+# sed -i 5000,+9999999999999999d title.principals.tsv na potrzeby testów
 hadoop fs -mkdir -p in
 hadoop fs -copyFromLocal title.principals.tsv in/
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -files reducer.py,mapper.py -mapper mapper.py -reducer reducer.py -input in\* -output out_python
